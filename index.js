8c12e4de415c0f3795d3113c4777115c8251f18a
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-03-25 06:40:34 -0400

    added a function to filter data in db.json but have to find a way to accept variable filtering options, and have to clear table and filteredArray on submit or else filtered elements just stay in the array and get rendered again on each submit. Currently elements are getting added to the end of the already populated table instead of replacing it

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 
-
-
-
-
 // ---------------------------------------------------------
 
 async function fetchElements() {
-  // To pass the tests, don't forget to return your fetch!
+  
   const res = await fetch("http://localhost:3000/elements");
     const elements = await res.json();
     return renderElements(elements);  
 }
 
 
+
 function renderElements(elements) {
   const tableBody = document.querySelector('.tableBody');
   //const table = document.querySelector('table');
+  //console.log(elements); 
 
   elements.forEach(element => {
     const tr = document.createElement('tr');
@@ -43,32 +41,91 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-
-
-
-
-let searchBy;
-let searchText;
+/*
+See comments in this function. Many things to fix.
+*/
+let filteredElements = [];   
 let searchBoxText;
-// idk if I want to use this yet.
-// used with the function html snippet in index.html
+function filter(){
+
+    let searchText = document.getElementById("searchText");
+    searchBoxText = searchText.value;  
+    console.log("option: " + option);
+    console.log(searchBoxText);
+    let match = false;
+    let e;
+      fetch(`http://localhost:3000/elements/`)
+        .then((response) => response.json())
+        .then((data) => {
+          //console.log(data);
+          //console.log(data[0]);
+          console.log(data[7]);
+          console.log(data[7].name);    // prints "oxygen"
+          console.log(option);          // prints "name"but...
+          console.log(data[7].option);  // ...this is undefined. 
+                                        // How do I access a property through a variable?
+          
+          
+
+
+          // Need to figure out how to use a variable to call the object property                       
+          for (let i =0; i < data.length; i++){
+            
+            //if (data[i].option === searchBoxText){        // returns no matches when I try to use the option variable containing the property name
+            if (data[i].name === searchBoxText){       // correctly finds matching elements
+              console.log("element is: " + data[i]);   // returns [object Object]
+              e = data[i];
+              filteredElements.push(e);               
+              console.log(data[i]);                   // returns correct element object when a literal property value is used 
+              match = true;
+            }         
+          }      
+        });
+
+        console.log("match found: " + match); // always prints false even though it goes through the if loop
+
+        // elements will appear as [object Object] but they do render fine
+        console.log("filteredElements: " + filteredElements); 
+        
+        renderElements(filteredElements); 
+        /* Only when I name the object property literally instead of using the variable "option"
+        
+        The elements are being rendered even though they are printing as [object Object]
+         in the console. But It does not show up in the table unless I submit the form again.
+         The filteredElements array needs to be cleared each time I hit the form, 
+         otherwise everything I search stays in the array, and is rendered again everytime I submit
+
+         I also need to clear the table everytime bc right now its all just being 
+         added to the end of the table.
+        
+        */
+      };
+  
+  
+
+
+
+
+/*  Commented out to test filter() function. May not need this one.
+
+let searchBoxText;
 function getform(){
-    
-    //searchBy = document.getElementById("searchBy").text;
-    searchText = document.getElementById("searchText");
-    //let option = searchBy.options[searchBy.selectedIndex].text;  
+
+    let searchText = document.getElementById("searchText");
     searchBoxText = searchText.value;  
     alert("you wrote: " + searchBoxText);
     
 }
+*/
 
 // Gets dropdown menu selection
-let menuSelection;
+
+let option;
 function menuHandler(){
-  menuSelection = document.querySelector('#searchBy');
+  let menuSelection = document.querySelector('#searchBy');
 
   // displays selection
-  let option = menuSelection.value;
+  option = menuSelection.value;
   document.querySelector('.output').textContent = option;
   console.log(option);
 }
