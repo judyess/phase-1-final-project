dd09e46f883e5d5848987bb72cb747b3a49d8179
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-03-26 12:58:53 -0400

    fixed issues with filter variables and rewrote renderElements() to populate the table with whatever the table header is, so I can add the option to create other groups to show more elemental properties. Need to fiure out how to add horizontal scroll bar to table so table doesn't squish as more properties are added

--- index.js
+++ index.js
@@ -11,94 +11,101 @@ async function fetchElements() {
 
 
 function renderElements(elements) {
+  
   const tableBody = document.querySelector('.tableBody');
-  //const table = document.querySelector('table');
-  //console.log(elements); 
 
   elements.forEach(element => {
     const tr = document.createElement('tr');
+    tr.setAttribute('class', 'rowData');
+    tableBody.appendChild(tr);
+    /*
     const td1 = document.createElement('td');
     const td2 = document.createElement('td');
     const td3 = document.createElement('td');
     const td4 = document.createElement('td');
+
     td1.innerHTML = element.number;
     td2.innerHTML = element.name;
     td3.innerHTML = element.abbreviation;
     td4.innerHTML = element.atomicMass;
-    tableBody.appendChild(tr);
-    //table.appendChild(tr);
+
+    
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
+    */
+
+    // I want to rewrite the above as a loop to create rows of data based on the number of tblheaders
+    /*
+    for each element, create a row.
+      for each tableHeader, create a column of data, 
+        const td = document.createElement('td');
+          populate that column of data with element.key, where key is whatever the tableHeaders ID is
+        let key = tableHeader.attr("id");
+        td.innerHTML = element.
+        
+
+  
+    */
+    const headers = document.getElementsByClassName("tblHeader");
+    
+    for(let i = 0; i < headers.length; i++){
+  
+      let td = document.createElement("td");
+      let key = headers[i].getAttribute('id');
+      tr.appendChild(td);
+      td.innerHTML = element[`${key}`]; 
+    }
+
   });
 }
 
+function clearTable() {
+  const rows = document.getElementsByClassName('rowData');
+  let size = rows.length;
+  for(let i =0; i< size; i++){
+    rows[0].remove();
+  }
+}
 
-// When the page loads, call fetchElements() to get data in db.json
+// When the page loads, populates the table with data from db.json
 document.addEventListener('DOMContentLoaded', function() {
   fetchElements();
 });
 
 
-/*
-See comments in this function. Many things to fix.
-*/
+
 let filteredElements = [];   
 let searchBoxText;
-function filter(){
+let option = "number";
 
+
+// abbreviation searches are case sensitive.
+function filter(opt){
     let searchText = document.getElementById("searchText");
     searchBoxText = searchText.value;  
-    console.log("option: " + option);
-    console.log(searchBoxText);
+
+    let key = `${opt}`;
+    console.log(key);
+
     let match = false;
     let e;
       fetch(`http://localhost:3000/elements/`)
         .then((response) => response.json())
-        .then((data) => {
-          //console.log(data);
-          //console.log(data[0]);
-          console.log(data[7]);
-          console.log(data[7].name);    // prints "oxygen"
-          console.log(option);          // prints "name"but...
-          console.log(data[7].option);  // ...this is undefined. 
-                                        // How do I access a property through a variable?
-          
-          
-
-
-          // Need to figure out how to use a variable to call the object property                       
+        .then((data) => {                 
           for (let i =0; i < data.length; i++){
-            
-            //if (data[i].option === searchBoxText){        // returns no matches when I try to use the option variable containing the property name
-            if (data[i].name === searchBoxText){       // correctly finds matching elements
-              console.log("element is: " + data[i]);   // returns [object Object]
+            if (data[i][`${key}`] == searchBoxText){      // Use only two ='s so that integer values will still match if sent as string   
               e = data[i];
+              console.log(e);
               filteredElements.push(e);               
-              console.log(data[i]);                   // returns correct element object when a literal property value is used 
               match = true;
             }         
-          }      
+          }  
+          renderElements(filteredElements);  // Have to render this inside the fetch to have the filtered elements appear. idk why?
+          filteredElements = []; // clears the array so filtered elements don't get rerendered again.
         });
 
-        console.log("match found: " + match); // always prints false even though it goes through the if loop
-
-        // elements will appear as [object Object] but they do render fine
-        console.log("filteredElements: " + filteredElements); 
-        
-        renderElements(filteredElements); 
-        /* Only when I name the object property literally instead of using the variable "option"
-        
-        The elements are being rendered even though they are printing as [object Object]
-         in the console. But It does not show up in the table unless I submit the form again.
-         The filteredElements array needs to be cleared each time I hit the form, 
-         otherwise everything I search stays in the array, and is rendered again everytime I submit
-
-         I also need to clear the table everytime bc right now its all just being 
-         added to the end of the table.
-        
-        */
       };
   
   
@@ -120,19 +127,20 @@ function getform(){
 
 // Gets dropdown menu selection
 
-let option;
+//let option = "Number";
 function menuHandler(){
   let menuSelection = document.querySelector('#searchBy');
 
   // displays selection
   option = menuSelection.value;
   document.querySelector('.output').textContent = option;
-  console.log(option);
+  //console.log(option);
 }
 
 
 // Stop page from refreshing
 var form=document.getElementById("filterForm");
+//var form2 = document.getElementById("form2");
 
 function submitForm(event){
    event.preventDefault();
@@ -140,3 +148,4 @@ function submitForm(event){
 
 // lists for a submit event on the filterForm only
 form.addEventListener('submit', submitForm);
+//form2.addEventListener('submit', submitForm);
--- load.js
+++ load.js
@@ -1,11 +1,8 @@
 // code I wrote for lab "phase-1-sending-data-with-fetch" used here 
 // along with formatted information I gathered from my chemistry SQL project 
 // This file just populates the db.json file with elemental data.
-// Should keep this so I can clean up db.json if I add functions 
-// for the user to create more groups and add to the table
 
 function submitData(number, name, abbreviation, atomicMass) {
-
     fetch("http://localhost:3000/elements", {
         method: "POST",
         headers: {
@@ -24,17 +21,95 @@ function submitData(number, name, abbreviation, atomicMass) {
             })
                 .then(function (object) {
                    return object.document.body.innerHTML = `<p>${response.id}</p>`
+                })
+                    .catch(function (error) {
+                        console.log("error", error);
+                        return (document.body.innerHTML = `<p>${error.message}</p>`);
+                    })                 
+}
+
+
+// function to add a property and value to an element in db.json. 
+// use PATCH to update the element objects
+
+// The function returns an error but still updates the element specified by its number.
+function addProperty(propertyName, value, number) {
+    // technically, I am accessing the element by its object ID and not its atomic Number
+    // they just happen to be the same.
+    // but try to figure out how to get to a specific object to update it when this is not the case
+    
+    fetch(`http://localhost:3000/elements/${number}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({    
+            [`${propertyName}`]: value,         
+        }),
+    })
+            .then(function (response) {
+                return response.json();
+            })
+                .then(function (object) {
+                   return object.document.body.innerHTML = `<p>${response.id}</p>`
 
                 })
                     .catch(function (error) {
                         console.log("error", error);
                         return (document.body.innerHTML = `<p>${error.message}</p>`);
                     })
-                    
 }
 
 
+// These arrays contain the atomic numbers of the elements that belong to the named period
+let period1 = [
+    1, 2
+]
+let period2 = [
+    3, 4, 5, 6, 7, 8, 9, 10
+]
+let period3 = [
+    11, 12, 13, 14, 15, 16, 17, 18
+]
+let period4 = [
+    19, 20, 21, 22, 23, 24, 25, 26, 36
+]
+let period5 = [
+    37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54
+]
+let period6 = [
+    55, 56, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86
+]
+let period7 = [
+    87, 88, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118
+]
 
+// this function takes one of the arrays written above,
+//  and finds the elements that are referenced, then calls addProperty() on them.
+function processArray(arr) {
+    fetch(`http://localhost:3000/elements/`)
+    .then((response) => response.json())
+    .then((data) => {
+                  
+      for (let i =0; i < data.length; i++){        
+        for( let j = 0; j < arr.length; j++){
+            if (data[i].number === arr[j]){    
+                addProperty("period", 1, arr[j]); // I have the propertyName and value hard coded. Change this. (!!!)
+            } 
+        }               
+      }      
+    });
+}
+
+
+processArray(period1);
+
+
+
+
+
+/*
 submitData(1, 'Hydrogen', 'H', 1.0079);
 submitData(2, 'Helium', 'He', 4.0026);
 submitData(3, 'Lithium', 'Li', 6.941);
@@ -153,3 +228,5 @@ submitData(115, 'Ununpentium', 'Uup', 'NULL');
 submitData(116, 'Ununhexium', 'Uuh', 'NULL');
 submitData(117, 'Ununseptium', 'Uus', 'NULL');
 submitData(118, 'Ununoctium', 'Uuo', 'NULL');
+
+*/
