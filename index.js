035716946d504252e4f62348f54c57007421541c
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-04-17 16:52:07 -0400

    added a range filter option

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let filteredElements = [];   
 let option = null; 
-let output = document.querySelector('.output');
+let rangeType = null;
 let elements = [];
+let min;
+let max;
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchElements();
@@ -32,26 +34,25 @@ function renderElements(arr=elements) {
   });
 }
 
-function filterList(){ 
-    output.textContent = "";
-          let start = 0;
-          let stop = elements.length;
-          let match = false;              
-          for (let i = start; i < stop; i++){
-            if (elements[i][`${option}`] == document.getElementById("searchText").value){   
-              filteredElements.push(elements[i]);               
-              match = true;
-            }         
-          } 
-          if(match===false){
-            output.textContent = "No Match Found";
-          } else {
-          clearTable();
-          renderElements(filteredElements); 
-          filteredElements = []; 
-          }
-        };
-      
+function filterList(start, stop){ 
+  let output = document.querySelector('.output');
+  output.textContent = "";
+  let match = false;              
+  for (let i = start; i < stop; i++){
+    //if (elements[i][`${option}`] == document.getElementById("searchText").value){   
+      filteredElements.push(elements[i]);               
+      match = true;
+    };         
+  
+  if(match===false){
+    output.textContent = "No Match Found";
+  } else {
+  clearTable();
+  renderElements(filteredElements); 
+  filteredElements = []; 
+  }
+}
+
 
 function clearTable() {
   const rows = document.getElementsByClassName('rowData');
@@ -66,7 +67,10 @@ function seeAll() {
   renderElements();
 }
 
-
+document.getElementById("rangeType").addEventListener("change", function(){
+  rangeType = document.getElementById("rangeType").value;
+  console.log(rangeType);
+});
 
 document.querySelector('#searchBy').addEventListener("change", function() {
   option = document.querySelector('#searchBy').value;
@@ -74,7 +78,21 @@ document.querySelector('#searchBy').addEventListener("change", function() {
 
 document.getElementById("filterForm").addEventListener('submit', function(event){
   event.preventDefault();
-  filterList();
+  let getMin = document.getElementById("minValue").value;
+  let getMax = document.getElementById("maxValue").value;
+  if(getMin){
+    min = getMin;
+  } else {
+    min = 0;
+  }
+  if(getMax){
+    max = getMax;
+  } else{
+    max = elements.length;
+  }
+  console.log(min);
+  console.log(max);
+  filterList(min, max);
 });
 
 
