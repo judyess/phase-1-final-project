782b24536f8f010f11f64304a2f2552f57e00b63
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-04-24 22:44:07 -0400

    removed comments, rewrote DOMContentLoaded event's callback from an anonymous function to an arrow function to reduce lines of code, rewrote some variable declarations from let to const

--- index.js
+++ index.js
@@ -1,66 +1,53 @@
-let option = null; // global because I don't want to call functions when value changes
+let option = null; 
 let elements = [];
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchElements();
-});
+document.addEventListener('DOMContentLoaded', () => fetchElements());
 
 async function fetchElements() {
   const res = await fetch("http://localhost:3000/elements");
-    const response = await res.json();
+    const response = await res.json(); 
     elements = response;
     return renderElements();  
 }
 
-
 function renderElements(arr=elements) { 
   const tableBody = document.querySelector('.tableBody');
   const headers = document.getElementsByClassName("tblHeader");
-
   arr.forEach(element => {
     const tr = document.createElement('tr');
     tr.setAttribute('class', 'rowData');
     tableBody.appendChild(tr);
-
-    // forEach
     for(let i = 0; i < headers.length; i++){ 
-      let td = document.createElement("td");
-      let key = headers[i].getAttribute('id');
+      const td = document.createElement("td");
+      const key = headers[i].getAttribute('id');
       tr.appendChild(td);
       td.innerHTML = element[`${key}`]; 
     }
   });
 }
 
-// use option. 
 function filterList(){ 
-  let filteredElements = [];  
-  let output = document.querySelector('.output'); 
-  output.textContent = "";
+  const filteredElements = []; 
+  const output = document.querySelector('.output');
   let match = false; 
-  
-  // forEach or .filter?
-  for (let i = 0; i < elements.length; i++){
-    if (elements[i][`${option}`] == document.getElementById("searchText").value){   
-      filteredElements.push(elements[i]);               
+  output.textContent = "";
+  elements.forEach(element=>{
+    if (element[`${option}`] == document.getElementById("searchText").value){   
+      filteredElements.push(element);               
       match = true;
-    }         
-  } 
-
-  // if/else okay?
+    }
+  });
   if(match===false){
     output.textContent = "No Match Found";
   } else {
-  clearTable();
-  renderElements(filteredElements); 
+    clearTable();
+    renderElements(filteredElements); 
   }
 };
 
 function clearTable() {
   const rows = document.getElementsByClassName('rowData');
-  let size = rows.length;
-
-  // forEach
+  const size = rows.length;
   for(let i =0; i< size; i++){
     rows[0].remove();
   }
@@ -71,25 +58,18 @@ function seeAll() {
   renderElements();
 }
 
-// get option
 document.querySelector('#searchBy').addEventListener("change", function() {
   option = document.querySelector('#searchBy').value;
 });
-
 document.getElementById("filterForm").addEventListener('submit', function(event){
   event.preventDefault();
   filterList();
 });
-
-
 document.getElementById("clearTable").addEventListener("click", function(event){
   event.preventDefault();
   clearTable();
 })
-
-
 document.getElementById("seeAll").addEventListener("click", function(event){
   event.preventDefault();
   seeAll();
 });
-
